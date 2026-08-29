f41cd9a09e40afb5a08254abd38e488508093f6d
Smaran Kudapa <smarankudapa@gmail.com>
2026-08-29 16:11:32 -0700

    Change sample 3D integral to a non-trivial example

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { TripleIntegralVisual } from "./components/scene/TripleIntegralVisual";
 import { useParsedIntegral } from "./hooks/useParsedIntegral";
 
 const SAMPLE_DOUBLE_LATEX = "\\int_{-1}^{1}\\int_{-\\sqrt{1-x^2}}^{\\sqrt{1-x^2}} x^2+y^2 \\, dy\\, dx";
-const SAMPLE_TRIPLE_LATEX = "\\int_{0}^{1}\\int_{0}^{1-x}\\int_{0}^{1-x-y} 1 \\, dz\\, dy\\, dx";
+const SAMPLE_TRIPLE_LATEX = "\\int_{-1}^{1}\\int_{x/2-1}^{x+1}\\int_{xy-1}^{xy+1} y \\, dz\\, dy\\, dx";
 
 function App() {
   const [latex, setLatex] = useState(SAMPLE_DOUBLE_LATEX);
